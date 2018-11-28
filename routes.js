391bce3531dfe852e5cea2e9ad539c4bfa234743
Jake Will <jakewwill2000@gmail.com>
2018-11-28 00:00:28 -0800

    Added two new routes - /get_sequence_from_numbers and /get_sequences_from_numbers

--- routes.js
+++ routes.js
@@ -11,6 +11,9 @@ var fs = require('fs')
  */
 var url = require('url');
 
+/* A module that allows us to parse parameters as JSON */
+var queryString = require("querystring");
+
 module.exports = function(app, passport) {
 	/* Set the index page to render index.ejs in public/views */
 	app.get('/', function(req, res) {
@@ -46,7 +49,7 @@ module.exports = function(app, passport) {
 		var obj = JSON.parse(fs.readFileSync('sequence_sample.json', 'utf8'));
 
 		/* Loop through every sequence in the JSON object and compare with the id
-		 * passes as a parameter. Return the sequence itself as a string if a match 
+		 * passed as a parameter. Return the sequence itself as a string if a match 
 		 * is found
 		 */
 		for (var i = 0; i < obj["sequences"].length; i++) {
@@ -61,5 +64,103 @@ module.exports = function(app, passport) {
 		/* At this point, no sequence has been found */
 		console.log("Error with GET request /get_sequence - no sequence found");
 		res.end("Error - sequence not found");
+	})
+
+	/* A get request to get a single sequence from 3 numbers passed as separate parameters.
+	 * Return value type is a string, not JSON
+	 */
+	app.get('/get_sequence_from_numbers', function(req, res) {
+		console.log("GET request received - /get_sequence_from_numbers");
+
+		/* Build the query for the request to access the parameters */
+		var url_parts = url.parse(req.url, true);
+		var query = url_parts.query;
+
+		/* The numbers of the sequence passed as parameters */
+		var num1 = req.query.num1
+		var num2 = req.query.num2
+		var num3 = req.query.num3
+
+		/* Create a JSON object from our sequences file */
+		var obj = JSON.parse(fs.readFileSync('sequence_sample.json', 'utf8'));
+
+		/* Loop through every sequence in the JSON object and compare with the numbers
+		 * passed as a parameter. Return the sequence itself as a string if a match 
+		 * is found
+		 */
+		for (var i = 0; i < obj["sequences"].length; i++) {
+			if (obj["sequences"][i]["num1"] == num1 &&
+				obj["sequences"][i]["num2"] == num2 &&
+				obj["sequences"][i]["num3"] == num3) {
+
+				console.log("GET request /get_sequence succeeded - returned sequence");
+
+				/* Return the sequence as a string */
+				res.end(obj["sequences"][i]["seq"])
+
+				return
+			}
+		}
+
+		/* At this point, no sequence has been found */
+		console.log("Error with GET request /get_sequence_from_numbers - no sequence found");
+		res.end("Error - sequence not found");
+
+		return
+	})
+
+	/* A get request to get a list of sequences from multiple lists of 3 numbers
+	 * passed as parameters in JSON format. Returns the list of sequences in
+	 * JSON format, instead of as strings
+	 */
+	app.get('/get_sequences_from_numbers', function(req, res) {
+		console.log("GET request received - /get_sequence_from_numbers");
+
+		/* Build the query for the request to access the parameters */
+		var url_parts = url.parse(req.url, true);
+		var query = url_parts.query;
+
+		/* The parameters are passed as a json object rather than a string, so
+		 * we need to parse the numbers list into a json object here
+		 */
+		var numbersJson = JSON.parse(query["numbers"])
+
+		/* Create a JSON object from our sequences file */
+		var obj = JSON.parse(fs.readFileSync('sequence_sample.json', 'utf8'));
+
+		/* Create a sequences json object to return, which we will populate in the 
+		 * following for loop
+		 */
+		var sequences = {
+
+		}
+
+		/* Loop through every sequence in the JSON object and compare with the numbers
+		 * passed as parameters from each item in the numbersJson object. If a match is
+		 * found, add the sequence itself into the sequences json object
+		 *
+		 * Note - I am using Object.keys(numbersJson).length to determine the size of
+		 * our json object, and will do this to determine the lengths of all json objects
+		 * in this function
+		 */
+		for (var i = 0; i < obj["sequences"].length; i++) {
+			for (var j = 0; j < Object.keys(numbersJson).length; j++) {
+				if (obj["sequences"][i]["num1"] == numbersJson[j]["num1"] &&
+					obj["sequences"][i]["num2"] == numbersJson[j]["num2"] &&
+					obj["sequences"][i]["num3"] == numbersJson[j]["num3"]) {
+
+					/* Add the sequence with the appropriate key to our sequences object */
+					sequences[Object.keys(sequences).length] = obj["sequences"][i]["seq"]
+				}
+			}
+		}
+
+		/* At this point, no sequence has been found */
+		console.log("Request ended - /get_sequences_from_numbers")
+
+		/* Return the sequences object as a string */
+		res.end(JSON.stringify(sequences));
+
+		return
 	})
 }
