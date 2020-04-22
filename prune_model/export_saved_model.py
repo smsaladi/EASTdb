14bd184352eeddf967d34c4fd3fe11e99e5c0fcc
Shyam Saladi <smsaladi@gmail.com>
2020-04-22 10:59:13 -0700

    models are saved empty... tf issue? this fixes it

--- prune_model/export_saved_model.py
+++ prune_model/export_saved_model.py
@@ -6,28 +6,50 @@ Convert Keras model to tensorflow format for serving
 
 import argparse
 
-import tensorflow.compat.v1 as tf
+import tensorflow.keras as keras
+
+from tensorflow.python.keras import backend as K
+from tensorflow.compat.v1 import saved_model
+
+# SavedModel builder business
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants, signature_constants
+from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
+
+from sgidspace.sgikeras.metrics import precision, recall, fmeasure
+
 
 def convert_tfserving(model_fn, export_path):
     """Converts a tensorflow model into a format for tensorflow_serving
-    
+
     `model_fn` is the model filename (hdf5 probably)
     `export_path` is the location to write to
-
     """
     # The export path contains the name and the version of the model
-    tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
-    model = tf.keras.models.load_model(model_fn)
+    K.set_learning_phase(0)  # Ignore dropout at inference
+    model = keras.models.load_model(model_fn,
+            custom_objects={'precision': precision,
+                            'recall': recall,
+                            'fmeasure': fmeasure})
+    saved_model.save(model, export_path)
+    return
 
-    # Fetch the Keras session and save the model
-    # The signature definition is defined by the input and output tensors
-    # And stored with the default serving key
-    with tf.keras.backend.get_session() as sess:
-        tf.saved_model.simple_save(
-            sess,
-            export_path,
+
+    # This doesn't work but may be necessary in the future...
+    # For builder business see:
+    # https://github.com/tensorflow/serving/issues/310#issuecomment-297015251
+
+    builder = saved_model_builder.SavedModelBuilder(export_path)
+    with K.get_session() as sess:
+        signature = predict_signature_def(
             inputs={'input_seq_batch': model.input},
             outputs={t.name: t for t in model.outputs})
+
+        builder.add_meta_graph_and_variables(
+            sess=sess,
+            tags=[tag_constants.SERVING],
+            signature_def_map={'predict': signature})
+        builder.save()
 
     return
 
